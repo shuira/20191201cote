8f89b99d0127c7551ddcfed0e5cc604028dd14d2
shuira <shuira@gmail.com>
2019-12-01 13:36:11 +0800

    fist

---
+++ solution1/classlib1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace classlib1
+{
+    public class Class1
+    {
+        public string Go(){
+            #if NET472
+             return "NET472";
+            #else
+             return ".NET Standard 2.0";
+            #endif
+        }
+    }
+}
